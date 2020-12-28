5fed49981eddf93d3c8604ed07a91e00fbf75938
jerry <1052557325@qq.com>
2020-12-28 23:12:08 +0800

    add basic content to header

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,12 +1,22 @@
 import React,{Component} from "react"
-import {HeaderWrapper,Logo,Nav} from "./style";
+import {HeaderWrapper,Logo,Nav,NavItem,NavSearch,Addition,Button} from "./style";
 
 class Header extends Component {
    render() {
        return (
            <HeaderWrapper>
                <Logo href='/'/>
-               <Nav/>
+               <Nav>
+                   <NavItem className='left active'>首页</NavItem>
+                   <NavItem className='left'>下载APP</NavItem>
+                   <NavItem className='right'>登陆</NavItem>
+                   <NavItem className='right'>Aa</NavItem>
+                   <NavSearch></NavSearch>
+               </Nav>
+               <Addition>
+                   <Button className='writing'>写文章</Button>
+                   <Button className='reg'>注册</Button>
+               </Addition>
            </HeaderWrapper>
        )
    }
