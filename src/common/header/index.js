ede43446d7e8542cf32d8a222aa8824eb1a3a529
jerry <1052557325@qq.com>
2021-01-10 22:14:58 +0800

    add SearchInfoBoard

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,19 +1,59 @@
 import React from "react"
 import {connect} from 'react-redux'
-import {HeaderWrapper,Logo,Nav,NavItem,NavSearch,Addition,Button} from "./style";
+import {
+    HeaderWrapper,
+    Logo,
+    Nav,
+    NavItem,
+    NavSearch,
+    Addition,
+    Button,
+    SearchInfo,
+    SearchInfoTitle,
+    SearchInfoSwitch,
+    SearchInfoItem,
+    SearchInfoList
+} from "./style";
 import * as actionCreator from './store/actionCreator'
 
-const Header = (props) =>{
-   return (<HeaderWrapper>
+const getListArea = (show) => {
+    if(show){
+        return (
+            <SearchInfo>
+                <SearchInfoTitle>热门搜索
+                    <SearchInfoSwitch>换一换</SearchInfoSwitch>
+                </SearchInfoTitle>
+                <SearchInfoList>
+                    <SearchInfoItem>教育</SearchInfoItem>
+                    <SearchInfoItem>教育</SearchInfoItem>
+                    <SearchInfoItem>教育</SearchInfoItem>
+                    <SearchInfoItem>教育</SearchInfoItem>
+                    <SearchInfoItem>教育</SearchInfoItem>
+                    <SearchInfoItem>教育</SearchInfoItem>
+                </SearchInfoList>
+            </SearchInfo>
+        );
+    }else{
+        return null
+    }
+}
+
+const Header = (props) => {
+    return (<HeaderWrapper>
         <Logo href='/'/>
         <Nav>
             <NavItem className='left active'>首页</NavItem>
             <NavItem className='left'>下载APP</NavItem>
             <NavItem className='right'>登陆</NavItem>
             <NavItem className='right'>Aa</NavItem>
-            <NavSearch className={props.focused ? 'focused': ''}
-                       onFocus={props.handleInputFocus} onBlur={props.handleInputBlur}
-            ></NavSearch>
+            <NavSearch className={props.focused ? 'focused' : ''} onFocus={props.handleInputFocus}
+                       onBlur={props.handleInputBlur}
+
+            >
+            </NavSearch>
+            <NavItem className='left'>
+                {getListArea(props.focused)}
+            </NavItem>
 
         </Nav>
         <Addition>
@@ -23,19 +63,19 @@ const Header = (props) =>{
     </HeaderWrapper>);
 }
 
-const mapStateToProps = (state)=>{
+const mapStateToProps = (state) => {
     return {
-        focused : state.get('header').get('focused')
+        focused: state.get('header').get('focused')
     }
 }
 const mapDispatchToProps = (dispatch) => {
-    return{
-        handleInputFocus(){
+    return {
+        handleInputFocus() {
             dispatch(actionCreator.searchFocus());
         },
-        handleInputBlur(){
+        handleInputBlur() {
             dispatch(actionCreator.searchBlur());
         }
     }
 }
-export default connect(mapStateToProps,mapDispatchToProps)(Header)
+export default connect(mapStateToProps, mapDispatchToProps)(Header)
