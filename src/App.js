6dfef23263eb9606499607ab9ebfaf7491f536a1
jerry <1052557325@qq.com>
2021-01-13 23:27:06 +0800

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import Header from "./common/header";
 import {Provider} from 'react-redux'
+import {BrowserRouter,Route} from "react-router-dom";
 import store from "./store";
-
+import Home from './pages/home'
+import Detail from './pages/detail'
 // provider组件 将store暴露出来，让下面的组件可以使用store
 function App() {
     return (
         <Provider store={store}>
-            <Header/>
+            <div>
+                <Header/>
+                <BrowserRouter>
+                    {/* exact 表示精确匹配*/}
+                    <Route path='/' exact component={Home}></Route>
+                    <Route path='/detail' exact component={Detail}></Route>
+                </BrowserRouter>
+            </div>
         </Provider>
     );
 }
