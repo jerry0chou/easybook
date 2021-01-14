45745022c7cf6a7df7beb287ba91c9dbba1bf966
jerry <1052557325@qq.com>
2021-01-14 23:21:01 +0800

    add small widgets at Home component

---
+++ src/pages/home/components/Topic.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react'
+import {TopicWrapper, TopicItem} from '../style'
+
+class Topic extends Component {
+    render() {
+        return (
+            <TopicWrapper>
+                <TopicItem>
+                    <img className='topic-img'
+                         src='https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3363295869,2467511306&fm=26&gp=0.jpg'/>
+                    社会热点
+                </TopicItem>
+                <TopicItem>
+                    <img className='topic-img'
+                         src='https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3363295869,2467511306&fm=26&gp=0.jpg'/>
+                    社会热点
+                </TopicItem>
+                <TopicItem>
+                    <img className='topic-img'
+                         src='https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3363295869,2467511306&fm=26&gp=0.jpg'/>
+                    社会热点
+                </TopicItem>
+                <TopicItem>
+                    <img className='topic-img'
+                         src='https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3363295869,2467511306&fm=26&gp=0.jpg'/>
+                    社会热点
+                </TopicItem>
+                <TopicItem>
+                    <img className='topic-img'
+                         src='https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3363295869,2467511306&fm=26&gp=0.jpg'/>
+                    社会热点
+                </TopicItem>
+
+            </TopicWrapper>
+        )
+    }
+}
+
+export default Topic
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,23 @@
 import React,{Component} from 'react'
-
+import Topic from "./components/Topic";
+import Writer from "./components/Writer";
+import List from "./components/List";
+import Recommend from "./components/Recommend";
+import {HomeWrapper,HomeLeft,HomeRight} from './style'
 class Home extends Component{
     render() {
         return (
-            <div>Home</div>
+            <HomeWrapper>
+                <HomeLeft>
+                    <img className='banner-img' src="https://img1.mukewang.com/5febfc0f0001674217920764.jpg"/>
+                    <Topic/>
+                    <List/>
+                </HomeLeft>
+                <HomeRight>
+                    <Recommend/>
+                    <Writer/>
+                </HomeRight>
+            </HomeWrapper>
         )
     }
 }
