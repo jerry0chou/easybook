1a32284403a05016cd1a7e9bd41b5fc62e210187
jerry <1052557325@qq.com>
2021-01-18 22:18:21 +0800

    add detail page

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,10 +1,25 @@
-import React,{PureComponent} from 'react'
+import React, {PureComponent} from 'react'
+import {DetailWrapper,Header,Content} from './style'
 
-class Detail extends PureComponent{
+class Detail extends PureComponent {
     render() {
         return (
-            <div>Detail</div>
+            <DetailWrapper>
+                <Header>
+                    戴望舒——在最美的时光里错过
+                </Header>
+                <Content>
+                    <img src='https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2083879881,1683628577&fm=26&gp=0.jpg'/>
+                    <p>你是否记得，那个在雨巷中独自哀愁的丁香一样的姑娘？戴望舒的一生，都在追寻那个有着丁香般芬芳的姑娘他凝望，他期盼，他等待，终究是一次又一次地错过那姑娘,那芬芳，一次又一次悔恨自责与幻想期待交织</p>
+                    <p>你是否记得，那个在雨巷中独自哀愁的丁香一样的姑娘？戴望舒的一生，都在追寻那个有着丁香般芬芳的姑娘他凝望，他期盼，他等待，终究是一次又一次地错过那姑娘,那芬芳，一次又一次悔恨自责与幻想期待交织</p>
+                    <p>你是否记得，那个在雨巷中独自哀愁的丁香一样的姑娘？戴望舒的一生，都在追寻那个有着丁香般芬芳的姑娘他凝望，他期盼，他等待，终究是一次又一次地错过那姑娘,那芬芳，一次又一次悔恨自责与幻想期待交织</p>
+                    <p>你是否记得，那个在雨巷中独自哀愁的丁香一样的姑娘？戴望舒的一生，都在追寻那个有着丁香般芬芳的姑娘他凝望，他期盼，他等待，终究是一次又一次地错过那姑娘,那芬芳，一次又一次悔恨自责与幻想期待交织</p>
+                    <p>你是否记得，那个在雨巷中独自哀愁的丁香一样的姑娘？戴望舒的一生，都在追寻那个有着丁香般芬芳的姑娘他凝望，他期盼，他等待，终究是一次又一次地错过那姑娘,那芬芳，一次又一次悔恨自责与幻想期待交织</p>
+
+                </Content>
+            </DetailWrapper>
         )
     }
 }
+
 export default Detail
