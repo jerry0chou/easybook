72240a7da50f254014243594cabdac21988f802c
jerry <1052557325@qq.com>
2021-01-18 22:54:05 +0800

    add store to detail component

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,25 +1,23 @@
 import React, {PureComponent} from 'react'
 import {DetailWrapper,Header,Content} from './style'
-
+import {connect} from 'react-redux'
 class Detail extends PureComponent {
     render() {
         return (
             <DetailWrapper>
                 <Header>
-                    戴望舒——在最美的时光里错过
+                    {this.props.title}
                 </Header>
-                <Content>
-                    <img src='https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2083879881,1683628577&fm=26&gp=0.jpg'/>
-                    <p>你是否记得，那个在雨巷中独自哀愁的丁香一样的姑娘？戴望舒的一生，都在追寻那个有着丁香般芬芳的姑娘他凝望，他期盼，他等待，终究是一次又一次地错过那姑娘,那芬芳，一次又一次悔恨自责与幻想期待交织</p>
-                    <p>你是否记得，那个在雨巷中独自哀愁的丁香一样的姑娘？戴望舒的一生，都在追寻那个有着丁香般芬芳的姑娘他凝望，他期盼，他等待，终究是一次又一次地错过那姑娘,那芬芳，一次又一次悔恨自责与幻想期待交织</p>
-                    <p>你是否记得，那个在雨巷中独自哀愁的丁香一样的姑娘？戴望舒的一生，都在追寻那个有着丁香般芬芳的姑娘他凝望，他期盼，他等待，终究是一次又一次地错过那姑娘,那芬芳，一次又一次悔恨自责与幻想期待交织</p>
-                    <p>你是否记得，那个在雨巷中独自哀愁的丁香一样的姑娘？戴望舒的一生，都在追寻那个有着丁香般芬芳的姑娘他凝望，他期盼，他等待，终究是一次又一次地错过那姑娘,那芬芳，一次又一次悔恨自责与幻想期待交织</p>
-                    <p>你是否记得，那个在雨巷中独自哀愁的丁香一样的姑娘？戴望舒的一生，都在追寻那个有着丁香般芬芳的姑娘他凝望，他期盼，他等待，终究是一次又一次地错过那姑娘,那芬芳，一次又一次悔恨自责与幻想期待交织</p>
-
+                <Content dangerouslySetInnerHTML={{__html:this.props.content}}>
                 </Content>
             </DetailWrapper>
         )
     }
 }
+const mapState = (state) =>({
+    title:state.getIn(['detail','title']),
+    content:state.getIn(['detail','content'])
+})
 
-export default Detail
+
+export default connect(mapState,null)(Detail)
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,7 +1,9 @@
 import  headerReducer from "../common/header/store/reducer";
 import {combineReducers} from "redux-immutable";
 import  homeReducer from '../pages/home/store/reducer'
+import detailReducer from '../pages/detail/store/reducer'
 export default combineReducers({
     header:headerReducer,
     home: homeReducer,
+    detail: detailReducer
 })
